3b226b846b1ad6d6131cdbc1fba132babdcc4468
Bittu Gurrala <bittugurrala241@gmail.com>
2025-11-17 18:29:19 +0530

    Fixed task list problem

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let addButton = document.getElementById("Add");
+let TodoInput = document.getElementById("Todoinput")
+let tasksContainer = document.getElementById("tasksContainer")
+
+addButton.addEventListener("click", function(){
+    let innerDiv = document.createElement("div") //Creating inner Div
+    let textValue = document.createElement("p");  //Creating P to put it in Div
+    let circle = document.createElement("p");   //Creating P for circle
+    textValue.textContent = TodoInput.value;
+    circle.classList.add("check")
+    innerDiv.classList.add("inner_div")
+    tasksContainer.appendChild(innerDiv);
+    innerDiv.appendChild(circle);
+    innerDiv.appendChild(textValue);
+    TodoInput.value= ""
+
+})
