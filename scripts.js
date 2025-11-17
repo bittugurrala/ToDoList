ebbde9c27f9c9ed4c7c5534a942da847e81984af
Bittu Gurrala <bittugurrala241@gmail.com>
2025-11-17 21:36:56 +0530

    Fixed task circle with span

--- scripts.js
+++ scripts.js
@@ -5,8 +5,9 @@ let tasksContainer = document.getElementById("tasksContainer")
 addButton.addEventListener("click", function(){
     let innerDiv = document.createElement("div") //Creating inner Div
     let textValue = document.createElement("p");  //Creating P to put it in Div
-    let circle = document.createElement("p");   //Creating P for circle
+    let circle = document.createElement("span");   //Creating P for circle
     textValue.textContent = TodoInput.value;
+    circle.innerHTML = `<i class="fa-regular fa-circle"></i>`
     circle.classList.add("check")
     innerDiv.classList.add("inner_div")
     tasksContainer.appendChild(innerDiv);
